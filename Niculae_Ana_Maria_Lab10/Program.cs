6671ccd0b6ecea290cbe4fc9e12e3e7c9165dd95
ANA-MARIA NICULAE <ana.niculae@ubbcluj.ro>
2024-01-11 09:56:09 +0200

    Add project files.

---
+++ Niculae_Ana_Maria_Lab10/Program.cs
@@ -0,0 +1,22 @@
+using Niculae_Ana_Maria_Lab10.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+app.Run();
